e6628323173e383996954dd2c9ba34400d90c717
Tatsiana Korshunava <tachichoco@yahoo.com>
2018-09-24 16:29:51 +0300

    first result

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    // your implementation
+    var countTriangles = 0;
+
+    for (i = 0; i < preferences.length; i++) {
+        var a = preferences[i];
+        var b = preferences[a-1];
+        var c = preferences[b-1];
+
+    }
+
+    return countTriangles;
 };
